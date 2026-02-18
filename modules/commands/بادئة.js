2b9dfb2992fc7747172d6c8a59feb8b296aadde3
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-02-18 23:04:43 +0200

    Update بادئة.js

--- modules/commands/بادئة.js
+++ modules/commands/بادئة.js
@@ -2,7 +2,7 @@ const { Threads } = require('../../database/database');
 
 module.exports = {
   config: {
-    name: 'بادئة', // اسم الأمر الجديد
+    name: 'بادئة',
     version: '1.0',
     author: 'Hridoy',
     countDown: 5,
@@ -14,28 +14,41 @@ module.exports = {
       en: '   {pn} [البادئة_الجديدة] أو اتركها فارغة للعمل بدون بادئة'
     },
   },
+
   onStart: async ({ api, event, args }) => {
     try {
+      const { threadID, messageID } = event;
       // لو لم يدخل المستخدم أي بادئة، سيتم العمل بدون بادئة
       let newPrefix = args[0] || '';
 
-      const threadData = Threads.get(event.threadID);
+      const threadData = Threads.get(threadID);
       if (!threadData) {
-        return api.sendMessage('تعذر العثور على بيانات المجموعة.', event.threadID);
+        return api.sendMessage('𑁍 ────────────── 𑁍\n❌ تعذر العثور على بيانات المجموعة.\n𑁍 ────────────── 𑁍', threadID, messageID);
       }
 
       threadData.settings.prefix = newPrefix;
-      Threads.set(event.threadID, threadData);
+      Threads.set(threadID, threadData);
+
+      let msg = `𑁍 ────────────── 𑁍\n`;
+      msg += `⊱ ✅ ⊰ تـم تـم ⊱ ✅ ⊰\n\n`;
 
       if (newPrefix === '') {
-        api.sendMessage('بادئة افتراضه! .', event.threadID);
+        msg += `𖤍 ┋ الـحـالـة: تـم التـصـفـيـر\n`;
+        msg += `𖦹 ┋ الـوصـف: الـعـمل بـدون بـادئـة\n`;
+        msg += `🔱 ┋ الـنـتـيـجـة: 『 نـجـاح 』\n`;
       } else {
-        api.sendMessage(`تم ضبط البادئة الى: 〘${newPrefix}〙.`, event.threadID);
+        msg += `𖤍 ┋ الـحـالـة: تـم الـضـبـط\n`;
+        msg += `𖦹 ┋ الـبـادئـة: 『 ${newPrefix} 』\n`;
+        msg += `🔱 ┋ الـنـتـيـجـة: 『 نـجـاح 』\n`;
       }
+
+      msg += `\n𑁍 ────────────── 𑁍`;
+
+      return api.sendMessage(msg, threadID, messageID);
 
     } catch (error) {
       console.error("خطأ في أمر تعيين البادئة:", error);
-      api.sendMessage('حدث خطأ أثناء ضبط البادئة.', event.threadID);
+      api.sendMessage('𑁍 ─ ❌ حـدث خـطأ فـي الـنظام ─ 𑁍', event.threadID);
     }
   },
 };
