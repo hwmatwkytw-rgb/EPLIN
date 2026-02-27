823c35eb314390a8049f0b1922c6c3b98adf5d80
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-02-27 23:10:03 +0200

    Update botJoinedGroup.js
    Signed-off-by: sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>

--- modules/events/botJoinedGroup.js
+++ modules/events/botJoinedGroup.js
@@ -1,29 +1,31 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+
 module.exports = {
   config: {
     name: 'botJoinedGroup',
-    version: '2.5',
+    version: '2.6',
     author: 'Hridoy / Minimal Style',
-    description: 'ترحيب انضمام البوت مع كنية احترافية وزخرفة الجوهرة.',
+    description: 'ترحيب انضمام البوت مع صورة وكنية احترافية.',
     eventType: ['log:subscribe'],
   },
 
   onStart: async ({ api, event }) => {
     try {
-      const { Threads } = require('../../database/database');
       const botID = api.getCurrentUserID();
       const addedParticipants = event.logMessageData?.addedParticipants || [];
 
+      // التأكد أن البوت هو العضو الجديد
       if (!addedParticipants.some(user => user.userFbId === botID)) return;
 
-      await Threads.create(event.threadID, 'New Group');
       const botName = global.client.config.botName || ' 𝕒𝕡𝕝𝕚𝕟';
       const prefix = global.client.config.prefix || '/';
 
-      // تغيير الكنية إلى الشكل الجديد: [ الرمز ] 𝕒𝕡𝕝𝕚𝕟🕸️
+      // تغيير الكنية
       const shortNickname = `[ ${prefix} ] 𝕒𝕡𝕝𝕚𝕟🕸️`;
       api.changeNickname(shortNickname, event.threadID, botID);
 
-      // زخرفة الجوهرة التي اخترتها
+      // زخرفة الترحيب
       const welcomeMsg = `
 ╭━─━─━─≪ ஜ▲ஜ ≫─━─━─━╮
       ▣ ${botName}  ▣
@@ -31,9 +33,7 @@ module.exports = {
 
   ◤ 🗝️ الـرمـز : ${prefix} ▣
   
-  
   ◤ 📜 الـدليل : ${prefix}help ▣
-  
   
   ◤ ๑ الـمطور : سينكو 17Y ▣
 
@@ -41,9 +41,20 @@ module.exports = {
       🌹 صـلّ علـى الـنـبي 🌹
 ╰━─━─━─≪ ஜ▼ஜ ≫─━─━─━╯`;
 
-      setTimeout(() => {
-        api.sendMessage(welcomeMsg, event.threadID);
-      }, 1000);
+      const imgPath = __dirname + "/cache/bot_join.jpg";
+      const imgUrl = "https://i.ibb.co/hJ47QQMM/1771089849331.jpg";
+
+      // تحميل الصورة وإرسالها
+      const response = await axios.get(imgUrl, { responseType: "arraybuffer" });
+      await fs.outputFile(imgPath, Buffer.from(response.data, "utf-8"));
+
+      api.sendMessage({
+        body: welcomeMsg,
+        attachment: fs.createReadStream(imgPath)
+      }, event.threadID, () => {
+        // حذف الصورة بعد الإرسال للحفاظ على مساحة التخزين
+        if (fs.existsSync(imgPath)) fs.unlinkSync(imgPath);
+      });
 
     } catch (err) {
       console.error('[ERROR]:', err);
