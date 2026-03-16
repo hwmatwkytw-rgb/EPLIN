64191cb96cbec2f517ac50042653d28c65f29114
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-03-16 22:37:04 +0200

    Update ايف.js
    Signed-off-by: sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>

--- modules/commands/ايف.js
+++ modules/commands/ايف.js
@@ -3,8 +3,8 @@ const util = require("util");
 module.exports = {
   config: {
     name: "ايف",
-    version: "2.0",
-    author: " x AbuUbaida",
+    version: "2.8",
+    author: "x AbuUbaida",
     countDown: 0,
     prefix: true,
     category: "owner",
@@ -12,16 +12,21 @@ module.exports = {
   },
 
   onStart: async function ({ api, event, args }) {
-    const OWNER_ID = "61588108307572"; // عدلها لو داير
+    const OWNER_ID = "61588108307572"; 
 
-    if (event.senderID !== OWNER_ID)
-      return api.sendMessage("❌ الأمر دا حق المطور بس.", event.threadID, event.messageID);
+    // لو العضو ما إنت، اتفاعل على رسالته بـ 🚯 واسكت
+    if (event.senderID !== OWNER_ID) {
+      return api.setMessageReaction("🚯", event.messageID, () => {}, true);
+    }
 
     const code = args.join(" ");
     if (!code)
-      return api.sendMessage("⚠️ اكتب كود بعد الأمر.", event.threadID, event.messageID);
+      return api.sendMessage("يس", event.threadID, event.messageID);
 
     try {
+      // تفاعل ليك إنت (الملك) بـ ✅ عشان تعرف إن الكود بدأ يتنفذ
+      api.setMessageReaction("⏳", event.messageID, () => {}, true);
+
       const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
 
       const result = await new AsyncFunction(
@@ -40,23 +45,31 @@ module.exports = {
         `
       )(api, event, require, process, console, global);
 
-      let output = typeof result === "string"
-        ? result
-        : util.inspect(result, { depth: 2 });
+      let output;
+      if (result === undefined) {
+          output =" ";
+      } else if (typeof result === "string") {
+          output = result;
+      } else {
+          output = util.inspect(result, { depth: 2 });
+      }
 
-      if (!output) output = "✅ Done";
+      if (output === "undefined") output = "";
 
-      // تقسيم لو طويل
       const max = 1900;
       for (let i = 0; i < output.length; i += max) {
         await api.sendMessage(
-          output.slice(i, i + max),
+          "\n" + output.slice(i, i + max),
           event.threadID
         );
       }
+      
+      // تغيير التفاعل لـ ✅ بعد النجاح
+      api.setMessageReaction("✅", event.messageID, () => {}, true);
 
     } catch (err) {
-      api.sendMessage("❌ Error:\n" + err, event.threadID, event.messageID);
+      api.sendMessage("⚠️ حصل كلاش:\n" + err.message, event.threadID, event.messageID);
+      api.setMessageReaction("❌", event.messageID, () => {}, true);
     }
   }
 };
